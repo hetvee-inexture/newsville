989beeb00b0d7c628bc2da890007c73d1e130dfa
Hetvee <you@example.com>
2020-06-23 09:08:30 +0530

    Add(views): Added views to news app

--- news/views.py
+++ news/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+def headlines(request):
+
+    return render(request, 'news/headlines.html')
+
+
+
+def state_news(request):
+
+
+    return render(request, 'news/state_news.html')
+
+
+
+def country_news(request):
+
+
+    return render(request, 'news/country_news.html')
+
+
+
+def sports_news(request):
+
+    return render(request, 'news/sports_news.html')
+
+
+
+def political_news(request):
+     
+     return render(request, 'news/political_news.html')
+
+
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('', hello, name='hello'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
