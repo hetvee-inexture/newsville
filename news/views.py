7732aeb77707e243342690fdb5453323598680f7
Hetvee <you@example.com>
2020-06-24 15:17:52 +0530

    Add(models): Added models to news

--- news/views.py
+++ news/views.py
@@ -1,30 +1,23 @@
 from django.shortcuts import render,redirect
 
+
 def headlines(request):
 
     return render(request, 'news/headlines.html')
-
-
 
 def state_news(request):
 
 
     return render(request, 'news/state_news.html')
 
-
-
 def country_news(request):
 
 
     return render(request, 'news/country_news.html')
 
-
-
 def sports_news(request):
 
     return render(request, 'news/sports_news.html')
-
-
 
 def political_news(request):
      
