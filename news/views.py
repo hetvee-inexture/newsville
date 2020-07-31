9baf996d56a7a022f68cc3f2b5d3590565165dc6
Hetvee <you@example.com>
2020-07-31 18:05:27 +0530

    feat(news): Added tag feature for rest of news

--- news/views.py
+++ news/views.py
@@ -45,22 +45,65 @@ def ndtv_news(request):
 		return render(request, 'news/ndtv_news.html', context)
 
 def zee_news(request):
-	# news_obj = ZeeNews.objects.all()
-	# if request.user.is_superuser:
-	# 	form = NewsTagForm()
+	news_obj = ZeeNews.objects.all()
 
-	# 	if request.method == 'POST':
-	# 		form = NewsTagForm(request.POST)
-	# 		if form.is_valid():
-	# 			tag = form.save(commit=False)
-	# 			tag.news_id = request.POST.get('news_id')
-	# 			tag.save()
-	# 			return redirect(/'')
+	if request.user.is_superuser:
+		form = NewsTagForm()
 
-	if request
+		if request.method == 'POST':
+			form = NewsTagForm(request.POST)
+			if form.is_valid():
+				tag = form.save(commit=False)
+				tag.news_id = request.POST.get('news_id')
+				tag.save()
+				return redirect('/zee-news')
+		
+		context = {
+			'latest_news' : news_obj,
+			'form' : form
+		}
+		return render(request, 'news/zee_news.html', context)
+
+	else:
+		tags = Tags.objects.all()
+		news_tags = NewsTag.objects.all()
+		context = {
+			'latest_news': news_obj,
+			'tags': tags,
+			'ids': news_tags
+		}
+		return render(request, 'news/zee_news.html', context)
 
 def scroll_news(request):
-	pass
+
+	news_obj = ScrollNews.objects.all()
+
+	if request.user.is_superuser:
+		form = NewsTagForm()
+
+		if request.method == 'POST':
+			form = NewsTagForm(request.POST)
+			if form.is_valid():
+				tag = form.save(commit=False)
+				tag.news_id = request.POST.get('news_id')
+				tag.save()
+				return redirect('/scroll-news')
+		
+		context = {
+			'latest_news' : news_obj,
+			'form' : form
+		}
+		return render(request, 'news/scroll_news.html', context)
+
+	else:
+		tags = Tags.objects.all()
+		news_tags = NewsTag.objects.all()
+		context = {
+			'latest_news': news_obj,
+			'tags': tags,
+			'ids': news_tags
+		}
+		return render(request, 'news/scroll_news.html', context)
 
 def add_tag(request):
 	form = TagForm()
