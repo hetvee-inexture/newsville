42e92ae8edca2bfb362504d16e5240774c57a765
Hetvee <you@example.com>
2020-08-10 10:31:46 +0530

    alter: Seperate ent news feature

--- news/urls.py
+++ news/urls.py
@@ -3,9 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home,name='home'),
-    path('ent-news/', ent_news, name='ent-news'),
-    # path('content/',content, name='home'),
-    path('search/', headlines_search, name='search'),
     path('add-tag/', add_tag, name='add-tag'),
     path('ndtv-news/', ndtv_news, name='ndtv-news'),
     path('zee-news/', zee_news, name='zee-news'),
